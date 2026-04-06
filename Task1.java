affa032a574715a54594a5bba58863c1b776312f
SIVASANKARMK <sivasankarmani76@gmail.com>
2026-04-06 15:44:22 +0530

    Fix class name casing for Task1 in Task1.java

--- Task1.java
+++ Task1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Task1 {
+public class task1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double value = sc.nextDouble();
