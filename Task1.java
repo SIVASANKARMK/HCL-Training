480d83a66edfb063c3850a9b57230eb9eec9c07a
SIVASANKARMK <sivasankarmani76@gmail.com>
2026-04-06 15:40:42 +0530

    Add Task1.java for discount calculation based on input value

---
+++ Task1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double value = sc.nextDouble();
+        double discount = 0;
+
+        if (value > 5000) {
+            discount = value * 0.20;
+        } 
+        else if (value >= 2000) { 
+            discount = value * 0.10;
+        } 
+        else {
+            discount = 0;
+        }
+
+        double finalAmount = value - discount; 
+        System.out.println("Discount: " + discount);
+        System.out.println("Final Price: " + finalAmount);
+    }
+}
